dc09a838d61b7d525eec46bdbb87a032d13bfb12
rafzby <rafal.zbytniewski@gmail.com>
2017-10-10 11:56:29 +0200

    Add program loader exception

--- src/vm/ProgramLoader.java
+++ src/vm/ProgramLoader.java
@@ -1,6 +1,7 @@
 package vm;
 
 import vm.exceptions.BadMemoryAccessException;
+import vm.exceptions.ProgramLoaderException;
 
 import java.io.DataInputStream;
 import java.io.FileInputStream;
@@ -10,20 +11,21 @@ import java.io.IOException;
 
 public class ProgramLoader {
     private static final int MEMORY_OFFSET = 0x200;
+
     private Memory memory;
+
 
     public ProgramLoader(Memory memory) {
         this.memory = memory;
     }
 
-    public boolean loadProgram(String fileName) {
+    public void loadProgram(String fileName) throws ProgramLoaderException {
         FileInputStream fileInputStream;
 
         try {
             fileInputStream = new FileInputStream(fileName);
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            return false;
+            throw new ProgramLoaderException("ROM file not found.");
         }
 
         DataInputStream inputStream = new DataInputStream(fileInputStream);
@@ -36,17 +38,10 @@ public class ProgramLoader {
                 memory.write(address, value);
                 address++;
             }
-        } catch (IOException | BadMemoryAccessException e) {
-            e.printStackTrace();
-            return false;
-        } finally {
-            try {
-                inputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        } catch (BadMemoryAccessException e) {
+            throw new ProgramLoaderException("Attempt to write data to the wrong memory address.");
+        } catch (IOException e) {
+            throw new ProgramLoaderException("Problem when reading data from ROM file.");
         }
-
-        return true;
     }
 }
---
+++ src/vm/exceptions/ProgramLoaderException.java
@@ -0,0 +1,7 @@
+package vm.exceptions;
+
+public class ProgramLoaderException extends Exception {
+    public ProgramLoaderException(String message) {
+        super(message);
+    }
+}

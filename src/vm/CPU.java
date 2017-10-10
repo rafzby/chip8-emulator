5d1f190f093f1429d7071aa40958727c3e7098a1
rafzby <rafal.zbytniewski@gmail.com>
2017-10-10 17:25:25 +0200

    Create and add RegisterManager to CPU

--- src/vm/CPU.java
+++ src/vm/CPU.java
@@ -3,27 +3,32 @@ package vm;
 import vm.exceptions.CpuException;
 import vm.exceptions.MemoryReadException;
 
+
 public class CPU {
     private static final int USER_PROGRAM_START_ADDRESS = 0x200;
     private static final int STACK_SIZE = 16;
+    private static final int REGISTERS_NUMBER = 16;
 
     private int programCounter;
     private Memory memory;
     private Stack stack;
+    private RegisterManager registerManager;
 
 
     public CPU(Memory memory) {
-        this.programCounter = USER_PROGRAM_START_ADDRESS;
         this.memory = memory;
 
         this.stack = new Stack(STACK_SIZE);
+        this.registerManager = new RegisterManager(REGISTERS_NUMBER);
+
+        this.programCounter = USER_PROGRAM_START_ADDRESS;
     }
 
     public void execute() throws CpuException {
         try {
             char opcode = memory.readOpcode(programCounter);
         } catch (MemoryReadException e) {
-            throw new CpuException("Unable to read opcode from memory");
+            throw new CpuException("Unable to read opcode from memory.");
         }
     }
 }
---
+++ src/vm/RegisterManager.java
@@ -0,0 +1,24 @@
+package vm;
+
+import java.util.ArrayList;
+
+public class RegisterManager {
+    private ArrayList<Register> registers;
+
+
+    public RegisterManager(int size) {
+        registers = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            registers.set(i, new Register());
+        }
+    }
+
+    public char getRegisterValue(int index) {
+        return registers.get(index).getValue();
+    }
+
+    public void setRegisterValue(int index, char value) {
+        registers.get(index).setValue(value);
+    }
+}

8da189054be1b1e915c70b4bf92bcbf8c856f44d
rafzby <rafal.zbytniewski@gmail.com>
2017-10-10 14:31:26 +0200

    Add basic register class

---
+++ src/vm/Register.java
@@ -0,0 +1,14 @@
+package vm;
+
+public class Register {
+    private char value;
+
+
+    public char getValue() {
+        return value;
+    }
+
+    public void setValue(char value) {
+        this.value = value;
+    }
+}

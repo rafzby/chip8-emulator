ff0e2f3d172f6c4b3b70e4596e3c9e21fd7a2542
rafzby <rafal.zbytniewski@gmail.com>
2017-10-10 20:22:48 +0200

    Fix type of program counter initial value

--- src/vm/CPU.java
+++ src/vm/CPU.java
@@ -4,7 +4,7 @@ import vm.exceptions.CpuException;
 import vm.exceptions.MemoryReadException;
 
 public class CPU {
-    private static final int USER_PROGRAM_START_ADDRESS = 0x200;
+    private static final char USER_PROGRAM_START_ADDRESS = 0x200;
     private static final int STACK_SIZE = 16;
     private static final int REGISTERS_NUMBER = 16;
 

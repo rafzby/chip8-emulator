aa6c6cb3ebea44057ede94a897555b03a044e635
rafzby <rafal.zbytniewski@gmail.com>
2017-10-10 18:29:37 +0200

    Remove initialize method and move font loading to the constructor of VirtualMachine class

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,6 @@ import vm.VirtualMachine;
 public class Main {
     public static void main(String[] args) {
         VirtualMachine virtualMachine = new VirtualMachine();
-
-        virtualMachine.initialize();
         virtualMachine.loadProgram("/home/rafzby/Projects/chip8-emulator/resources/IBM Logo.c8");
         virtualMachine.run();
     }
--- src/vm/VirtualMachine.java
+++ src/vm/VirtualMachine.java
@@ -6,34 +6,40 @@ import vm.exceptions.ProgramLoaderException;
 
 public class VirtualMachine {
     private static final int MEMORY_SIZE = 0x1000;
+    private static final int DISPLAY_SIZE = 0x800;
 
     private Memory memory;
     private CPU cpu;
     private FontLoader fontLoader;
     private ProgramLoader programLoader;
+    private Display display;
 
 
     public VirtualMachine() {
         memory = new Memory(MEMORY_SIZE);
+        display = new Display(DISPLAY_SIZE);
+
         cpu = new CPU(memory);
 
         fontLoader = new FontLoader(memory);
         programLoader = new ProgramLoader(memory);
-    }
 
-    public void initialize() {
-        try {
-            fontLoader.loadFont();
-        } catch (FontLoaderException e) {
-            e.printStackTrace();
-            System.exit(0);
-        }
+        loadFont();
     }
 
     public void loadProgram(String fileName) {
         try {
             programLoader.loadProgram(fileName);
         } catch (ProgramLoaderException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    private void loadFont() {
+        try {
+            fontLoader.loadFont();
+        } catch (FontLoaderException e) {
             e.printStackTrace();
             System.exit(0);
         }

63d75b223d46cfd8507426eb11e3ad2a9b8c5fc2
rafzby <rafal.zbytniewski@gmail.com>
2017-10-11 20:05:33 +0200

    Add loading ROM file from program arguments

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import vm.VirtualMachine;
 
-
 public class Main {
     public static void main(String[] args) {
-        VirtualMachine virtualMachine = new VirtualMachine();
-        virtualMachine.loadProgram("/home/rafzby/Projects/chip8-emulator/resources/IBM Logo.c8");
-        virtualMachine.start();
+        if(args.length != 0) {
+            VirtualMachine virtualMachine = new VirtualMachine();
+            virtualMachine.loadProgram(args[0]);
+            virtualMachine.start();
+        } else {
+            System.err.println("Missing parameter with ROM path.");
+        }
     }
 }

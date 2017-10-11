af0598e4bc7c91fd084d91acb1437501c70a0abe
rafzby <rafal.zbytniewski@gmail.com>
2017-10-11 13:05:18 +0200

    Add Keyboard class

---
+++ src/vm/Keyboard.java
@@ -0,0 +1,55 @@
+package vm;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class Keyboard extends KeyAdapter {
+    private static final int[] KEY_MAP= {
+            KeyEvent.VK_4, // 1
+            KeyEvent.VK_5, // 2
+            KeyEvent.VK_6, // 3
+            KeyEvent.VK_7, // 4
+            KeyEvent.VK_R, // 5
+            KeyEvent.VK_Y, // 6
+            KeyEvent.VK_U, // 7
+            KeyEvent.VK_F, // 8
+            KeyEvent.VK_G, // 9
+            KeyEvent.VK_H, // A
+            KeyEvent.VK_J, // B
+            KeyEvent.VK_V, // C
+            KeyEvent.VK_B, // D
+            KeyEvent.VK_N, // E
+            KeyEvent.VK_M, // F
+    };
+
+    private int currentKeyPerssed;
+
+
+    public Keyboard() {
+        currentKeyPerssed = 0;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+        currentKeyPerssed = mapKeycodeToChip8Key(keyEvent.getKeyCode());
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+        currentKeyPerssed = 0;
+    }
+
+    public int getCurrentKeyPerssed() {
+        return currentKeyPerssed;
+    }
+
+    private int mapKeycodeToChip8Key(int keycode) {
+        for (int i = 0; i < KEY_MAP.length; i++) {
+            if (KEY_MAP[i] == keycode) {
+                return i + 1;
+            }
+        }
+
+        return 0;
+    }
+}

4377808fdfca9a0ffe166bb7fdaf41bb9134262d
rafzby <rafal.zbytniewski@gmail.com>
2017-10-11 14:08:38 +0200

    Add IODevices interface and remove CPUCallbacks interface

--- src/vm/CPUCallbacks.java
+++
@@ -1,8 +0,0 @@
-package vm;
-
-public interface CPUCallbacks {
-    void onDisplayRepaint();
-    void onDisplayClear();
-    void onDisplaySetPixelValue(int position, int value);
-    int onDisplayGetPixelValue(int position);
-}
---
+++ src/vm/IODevices.java
@@ -0,0 +1,10 @@
+package vm;
+
+public interface IODevices {
+    void clearDisplay();
+    void repaintDisplay();
+    void setPixelValue(int position, int value);
+    int getPixelValue(int position);
+    int getCurrentKeyPressed();
+    boolean isKeyPressed(int key);
+}
